ae3c89e267c506fe02af56286b880a6ab3f6ece0
Prateek Mahendrakar <prateek.mahendrakar@gmail.com>
2019-02-05 14:48:57 -0500

    updated with latest

--- Canned Comments/canned_comments_json.js
+++ Canned Comments/canned_comments_json.js
@@ -1,70 +1,106 @@
 callback([
-		{
-			"name": "[Q] More than one question asked",
-			"description": "It is preferred if you can post separate questions instead of combining your questions into one. That way, it helps the people answering your question and also others hunting for at least one of your questions. Thanks!"
-		},
-		{
-			"name": "[Q] Duplicate Closure",
-			"description": "This question will probably be closed as a duplicate soon. If the answers from the duplicates don't fully address your question please edit it to include why and flag this for re-opening. Thanks!"
-		},
-		{
-			"name": "[A] Answers just to say Thanks!",
-			"description": "Please don't add \"thanks\" as answers. Invest some time in the site and you will gain sufficient <a href=\"//$SITEURL$/privileges\">privileges</a> to upvote answers you like, which is the $SITENAME$ way of saying thank you."
-		},
-		{
-			"name": "[A] Nothing but a URL (and isn't spam)",
-			"description": "Whilst this may theoretically answer the question, <a href=\"//meta.stackoverflow.com/q/8259\">it would be preferable</a> to include the essential parts of the answer here, and provide the link for reference."
-		},
-		{
-			"name": "[A] Requests to OP for further information",
-			"description": "This is really a comment, not an answer. With a bit more rep, <a href=\"//$SITEURL$/privileges/comment\">you will be able to post comments</a>. For the moment I've added the comment for you, and I'm flagging this post for deletion."
-		},
-		{
-			"name": "[A] OP using an answer for further information",
-			"description": "Please use the <em>Post answer</em> button only for actual answers. You should modify your original question to add additional information."
-		},
-		{
-			"name": "[A] OP adding a new question as an answer",
-			"description": "If you have another question, please ask it by clicking the <a href=\"//$SITEURL$/questions/ask\">Ask Question</a> button."
-		},
-		{
-			"name": "[A] Another user adding a 'Me too!'",
-			"description": "If you have a NEW question, please ask it by clicking the <a href=\"//$SITEURL$/questions/ask\">Ask Question</a> button. If you have sufficient reputation, <a href=\"//$SITEURL$/privileges/vote-up\">you may upvote</a> the question. Alternatively, \"star\" it as a favorite and you will be notified of any new answers."
-		},
-		{
-			"name": "[C] Too localized",
-			"description": "This question appears to be off-topic because it is too localized."
-		},
-		{
-			"name": "[EQ] Improper tagging",
-			"description": "The tags you were using are not appropriate for this question. Please review <a href=\"//$SITEURL$/help/tagging\">What are tags, and how should I use them?</a>"
-		},
-		{
-			"name": "[Q] Website is \" broken \", no code in question, just link to site",
-			"description": "Please add meaningful code and a problem description here. Don't just link to the site that needs fixing — otherwise, this question will lose any value to future visitors once the problem is solved or if the site you're linking to is inaccessible. Posting a [Minimal, Complete, and Verifiable example (MCVE)](https://stackoverflow.com/help/mcve) that demonstrates your problem would help you get better answers. For more info, see [Something on my web site doesn't work. Can I just paste a link to it?](https://meta.stackexchange.com/questions/125997/) Thanks!"
-		},
-		{
-			"name": "[Q] Needs to demonstrate research",
-			"description": "Sharing your research helps everyone. Tell us what you've tried and why it didn’t meet your needs. This demonstrates that you’ve taken the time to try to help yourself, it saves us from reiterating obvious answers, and most of all it helps you get a more specific and relevant answer! See also: [ask]"
-		},
-		{
-			"name": "[Q] Write my Code for Me",
-			"description": "Pure code-writing requests are off-topic on Stack Overflow — we expect questions here to relate to *specific* programming problems — but we will happily help you write it yourself! Tell us [what you've tried](https://stackoverflow.com/help/how-to-ask), and where you are stuck. This will also help us answer your question better."
-		},
-		{
-			"name": "[Q] Insufficient Code Example",
-			"description": "Questions seeking debugging help (why isn't this code working?) must include the desired behavior, a specific problem or error and the shortest code necessary to reproduce it **in the question itself**. Questions without a **clear problem statement** are not useful to other readers. See: [mcve]"
-		},
-		{
-			"name": "[C] Self-answers in a comment",
-			"description": "You should post this as an answer and accept it so others can see that your problem was solved, especially if you solved it yourself :)"
-		},
-		{
-			"name": "[A] Asking why this is not working on a Answer",
-			"description": "This does not provide an answer to the question. You can [search for similar questions](https://stackoverflow.com/search), or refer to the related and linked questions on the right-hand side of the page to find an answer. If you have a related but different question, [ask a new question](https://stackoverflow.com/questions/ask), and include a link to this one to help provide context. See: [Ask questions, get answers, no distractions](https://stackoverflow.com/tour)"
-		},
-		{
-			"name": "[A] Posting a link without any explanation as answer",
-			"description": "While this link may answer the question, it is better to include the essential parts of the answer here and provide the link for reference. Link-only answers can become invalid if the linked page changes."
-		}
-	])
+{
+	"name": "[Q] More than one question asked",
+	"description": "It is preferred if you can post separate questions instead of combining your questions into one. That way, it helps the people answering your question and also others hunting for at least one of your questions. Thanks!"
+},
+{
+	"name": "[Q] Duplicate Closure",
+	"description": "This question will probably be closed as a duplicate soon. If the answers from the duplicates don't fully address your question please edit it to include why and flag this for re-opening. Thanks!"
+},
+{
+	"name": "[A] Answers just to say Thanks!",
+	"description": "Please don't add \"thanks\" as answers. Invest some time in the site and you will gain sufficient <a href=\"//$SITEURL$/privileges\">privileges</a> to upvote answers you like, which is the $SITENAME$ way of saying thank you."
+},
+{
+	"name": "[A] Nothing but a URL (and isn't spam)",
+	"description": "Whilst this may theoretically answer the question, <a href=\"//meta.stackoverflow.com/q/8259\">it would be preferable</a> to include the essential parts of the answer here, and provide the link for reference."
+},
+{
+	"name": "[A] Requests to OP for further information",
+	"description": "This is really a comment, not an answer. With a bit more rep, <a href=\"//$SITEURL$/privileges/comment\">you will be able to post comments</a>. For the moment I've added the comment for you, and I'm flagging this post for deletion."
+},
+{
+	"name": "[A] OP using an answer for further information",
+	"description": "Please use the <em>Post answer</em> button only for actual answers. You should modify your original question to add additional information."
+},
+{
+	"name": "[A] OP adding a new question as an answer",
+	"description": "If you have another question, please ask it by clicking the <a href=\"//$SITEURL$/questions/ask\">Ask Question</a> button."
+},
+{
+	"name": "[A] Another user adding a 'Me too!'",
+	"description": "If you have a NEW question, please ask it by clicking the <a href=\"//$SITEURL$/questions/ask\">Ask Question</a> button. If you have sufficient reputation, <a href=\"//$SITEURL$/privileges/vote-up\">you may upvote</a> the question. Alternatively, " star " it as a favorite and you will be notified of any new answers."
+},
+{
+	"name": "[C] Too localized",
+	"description": "This question appears to be off-topic because it is too localized."
+},
+{
+	"name": "[EQ] Improper tagging",
+	"description": "The tags you were using are not appropriate for this question. Please review <a href=\"//$SITEURL$/help/tagging\">What are tags, and how should I use them?</a>"
+},
+{
+	"name": "[Q] Website is \"broken\", no code in question, just link to site",
+	"description": "Please add meaningful code and a problem description here. Don't just link to the site that needs fixing — otherwise, this question will lose any value to future visitors once the problem is solved or if the site you're linking to is inaccessible. Posting a [Minimal, Complete, and Verifiable example (MCVE)](https://stackoverflow.com/help/mcve) that demonstrates your problem would help you get better answers. For more info, see [Something on my web site doesn't work. Can I just paste a link to it?](https://meta.stackexchange.com/questions/125997/) Thanks!"
+},
+{
+	"name": "[Q] Needs to demonstrate research",
+	"description": "Sharing your research helps everyone. Tell us what you've tried and why it didn’t meet your needs. This demonstrates that you’ve taken the time to try to help yourself, it saves us from reiterating obvious answers, and most of all it helps you get a more specific and relevant answer! See also: [ask]"
+},
+{
+	"name": "[Q] Write my Code for Me",
+	"description": "Pure code-writing requests are off-topic on Stack Overflow — we expect questions here to relate to *specific* programming problems — but we will happily help you write it yourself! Tell us [what you've tried](https://stackoverflow.com/help/how-to-ask), and where you are stuck. This will also help us answer your question better."
+},
+{
+	"name": "[Q] Insufficient Code Example",
+	"description": "Questions seeking debugging help (why isn't this code working?) must include the desired behavior, a specific problem or error and the shortest code necessary to reproduce it **in the question itself**. Questions without a **clear problem statement** are not useful to other readers. See: [mcve]"
+},
+{
+	"name": "[C] Self-answers in a comment",
+	"description": "You should post this as an answer and accept it so others can see that your problem was solved, especially if you solved it yourself :)"
+},
+{
+	"name": "[A] Asking why this is not working on a Answer",
+	"description": "This does not provide an answer to the question. You can [search for similar questions](https://stackoverflow.com/search), or refer to the related and linked questions on the right-hand side of the page to find an answer. If you have a related but different question, [ask a new question](https://stackoverflow.com/questions/ask), and include a link to this one to help provide context. See: [Ask questions, get answers, no distractions](https://stackoverflow.com/tour)"
+},
+{
+	"name": "[A] Posting a link without any explanation as answer",
+	"description": "While this link may answer the question, it is better to include the essential parts of the answer here and provide the link for reference. Link-only answers can become invalid if the linked page changes."
+},
+{
+	"name": "[A] Only code - No explanation",
+	"description": "While this might answer the authors question, it lacks some explaining words and/or links to documentation.Raw code snippets are not very helpful without some phrases around them. You may also find how to write a <a href=\"https://$SITEURL$/help/how-to-answer\">good answer</a> very helpful."
+},
+{
+	"name": "[Q] No code only opinion",
+	"description": "This forum is meant more for solving specific problems with your code, and is not really a place to ask for general advice. You can check out what's on topic for Stackoverflow here: <a href=\"https://$SITEURL$/help/on-topic\">On topic</a>"
+},
+{
+	"name": "[Q] Missing info",
+	"description": "Please [edit] your question to [type here]. Thanks for improving the question's reference value and making it more answerable!"
+},
+{
+	"name": "[Q] Needs reduction",
+	"description": "That looks like more code than strictly needed. Can you reduce the code further until no code can be removed while still running into the problem?"
+},
+{
+	"name": "[Q] Self-answers in a comment/question",
+	"description": "You should post this as an answer and accept it so others can see that your problem was solved, especially if you solved it yourself."
+},
+{
+	"name": "[Q] No code in question",
+	"description": "Please [edit] to add meaningful code and a problem description here. Posting a <a href=\"http://$SITEURL$/help/mcve\">Minimal, Complete, Verifiable Example</a> that demonstrates your problem would help you get better answers. Thanks!"
+},
+{
+	"name": "[Q] Website is \"broken\", no code in question, just link to site",
+	"description": "Please [edit] your question to add meaningful code and a problem description here. Don't just link to the site that needs fixing - otherwise, this question will lose any value to future visitors once the problem is solved. Posting a <a href=\"http://$SITEURL$/help/mcve\">Minimal, Complete, Verifiable Example</a> that demonstrates your problem would help you get better answers. For more info, see <a href=\"http://meta.stackexchange.com/questions/125997/\">Something on my web site doesn't work. Can I just paste a link to it?</a> Thanks!"
+},
+{
+	"name": "Wrong language",
+	"description": "<a href=\"http://blog.stackoverflow.com/2009/07/non-english-question-policy/\">$SITENAME$ generally works in English</a>; please make sure your post is as useful as possible to others by translating it."
+},
+{
+	"name": "[A] Code-only",
+	"description": "Generally, answers are much more helpful if they include an explanation of what the code is intended to do, and why that solves the problem without introducing others."
+}]
+)
